a9ac48bd080f69f4a1f773e481b1e1b1183b286e
JeremyFJ <jjeremy1@vt.edu>
2024-09-26 14:53:05 +0000

    sep26

--- app.R
+++ app.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(shinyMobile)
 library(shinyTime)
+library(shinymanager)
 library(RPostgreSQL)
 library(DBI)
 library(dplyr)
@@ -10,127 +11,16 @@ library(DT)
 library(leaflet)
 library(leaflet.extras)
 library(htmltools)
+library(digest)
 
-current_date <- Sys.Date()
-tunisian_cities <- read.csv("./www/port_positions.csv")
+source("R_scripts/tabs.R")
+source("R_scripts/functions.R")
+
+tunisian_cities <- read.csv("./www/data/port_positions.csv")
 gear_list = data.frame(gear = c("Purse Seines", "Trawlers", "Drifting Longlines",
                               "Set Longlines", "Tuna Purse Seines",
                               "Set Gillnets", "Fixed Gear", "Dredge Fishing",
                               "Other"))
-# Function to connect to the pelagic database
-connectPelagic <- function() {
-  dbConnect(PostgreSQL(), user = 'spr', password = 'spr_pass', dbname = 'med_monitoring', host = 'localhost', port = 5432)
-}
-
-# Fetch distinct species names from taxonomy3 table
-fetchSpeciesNames <- function() {
-  con <- connectPelagic()
-  if (!is.null(con)) {
-    query <- "SELECT DISTINCT main_common_name FROM taxonomy3 ORDER BY main_common_name"
-    species_data <- dbGetQuery(con, query)
-    species_names <- c("", species_data$main_common_name)
-    dbDisconnect(con)
-    return(species_names)
-  }
-  return(character(0))
-}
-
-# Custom CSS to fix the dropdown issue, add borders to input fields, style the navbar, and center form inputs
-customCSS <- "
-.dropdown-menu {
-  z-index: 9999 !important;
-}
-
-.selectize-input input {
-  color: #000000 !important; /* Change text color to black */
-}
-
-.shiny-input-container {
-  padding-left: 20px;  /* Adjust this value as needed */
-  margin-bottom: 15px; /* Add margin between input fields */
-}
-
-.shiny-input-container input, 
-.shiny-input-container select, 
-.shiny-input-container textarea {
-  border: 2px solid #ddd !important;
-  padding: 10px;
-  border-radius: 5px;
-}
-
-#submit {
-  display: block; /* Make sure the button is a block-level element */
-  margin: 0 auto; /* Auto margins on the left and right */
-  width: 50%; /* Adjust the width as needed */
-  height: 60px; /* Adjust the height as needed */
-  font-size: 20px; /* Increase the font size of the button text */
-}
-
-/* Optional: Custom CSS to style the image */
-.top-image {
-  width: 300px;
-  height: auto;
-  display: block;
-  margin-left: 0; /* Align the image to the left */
-  margin-right: auto;
-  padding: 5px; /* Adjust or remove padding as needed */
-  border-radius: 10px; /* This creates rounded corners */
-}
-
-.maps-image {
-  max-width: 600px;
-  height: auto;
-  display: block;
-  margin-left: 0; /* Align the image to the left */
-  margin-right: auto;
-  padding: 5px; /* Adjust or remove padding as needed */
-  border-radius: 10px; /* This creates rounded corners */
-}
-
-.news-item figcaption {
-  max-width: 250px; /* Match the max-width of the image */
-  word-wrap: break-word; /* Ensure long words are broken to fit the width */
-  margin-left: 0; /* Align the caption with the image */
-  padding: 5px; /* Optional: add padding for better readability */
-}
-
-/* Custom CSS for navbar */
-.navbar-nav > li > a {
-  padding-top: 10px;
-  padding-bottom: 10px;
-}
-
-.navbar {
-  background-color: white !important;
-}
-
-.navbar-brand {
-  line-height: 30px;
-  height: 60px;
-  padding: 1 15px;
-}
-
-.navbar-right {
-  height: 30px;
-  display: flex;
-  align-items: center;
-}
-
-.navbar-right img {
-  height: 40px;
-}
-
-/* Centering form inputs */
-.centered-form {
-  display: flex;
-  justify-content: left;
-  align-items: left;
-}
-
-.centered-form .f7-card {
-  width: 100%;
-}
-"
 
 Sys.setenv(TZ = "America/New_York")
 current_time <- Sys.time()
@@ -142,10 +32,9 @@ ui <- navbarPage(
   title = tags$div(
     class = "navbar-right",
     tags$img(src = "tunisia-flag-round-shape-png.png"),
-    "Tunisia Fisheries App"
+    "Tunisia Fisheries Platform"
     ),
-  tags$head(
-    tags$style(HTML(customCSS)),
+  tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "styles.css"),
     tags$script(HTML("
       $(document).on('click', 'a[href^=\"#\"]', function(event) {
         event.preventDefault();
@@ -158,6 +47,7 @@ ui <- navbarPage(
     title = "Home",
     icon = icon("home"),
     fluidPage(
+      uiOutput("user_info"),
       h2("Welcome to the Tunisian App for Fisheries Monitoring!"),
       h2("Bienvenue sur l'application tunisienne de surveillance des pêches!"),
       p("Use the navigation bar to access different sections of the app."),
@@ -168,12 +58,15 @@ ui <- navbarPage(
       p("يوفر هذا التطبيق تقارير عن الصيد للصيادين التونسيين وخرائط نشاط السفن والأخبار الناشئة في مبادرات الشفافية في المحيطات."),
       tags$ul(
         tags$li(tags$a(href = "#NewCatch", "New Catch - Log your fishing data")),
-        tags$li(tags$a(href = "#Maps", "Maps - View real-time fishing activity"))
+        tags$li(tags$a(href = "#Maps", "Maps - View real-time fishing activity")),
+        tags$li(tags$a(href = "#TunisianSpecies", "Tunisian Species - Learn about fish species caught in Tunisia")),
+        tags$li(tags$a(href = "#Observers", "Observers - If you are monitoring ports, report what you are seeing!")),
+        tags$li(tags$a(href = "#User", "User Login - Register and login on our platform"))
       ),
       br(),
       h4("Recent News"),
       fluidRow(
-        column(4,
+        column(3,
           tags$div(
             class = "news-item",
             tags$a(
@@ -183,7 +76,17 @@ ui <- navbarPage(
             )
           )
         ),
-        column(4,
+        column(3,
+          tags$div(
+            class = "news-item",
+            tags$a(
+              href = "https://english.elpais.com/international/2024-01-14/the-blue-crab-in-tunisia-from-invasive-threat-to-godsend.html",
+              tags$img(src = "https://imagenes.elpais.com/resizer/v2/P5QDZXOSCJCUPOYCGQSXBDEJIA.jpg?auth=3adf3e37394c85677e1914b16280a6b0c0aa9437b5d90dd704eb26c53623c27d&width=1200", class = "top-image"),
+              tags$figcaption("The Blue Crab in Tunisia: From Invasive Threat to Godsend - El País")
+            )
+          )
+        ),
+        column(3,
           tags$div(
             class = "news-item",
             tags$a(
@@ -193,7 +96,7 @@ ui <- navbarPage(
             )
           )
         ),
-        column(4,
+        column(3,
           tags$div(
             class = "news-item",
             tags$a(
@@ -213,47 +116,50 @@ ui <- navbarPage(
     icon = icon("pencil-square"),
     value = "NewCatch", # Value for tab navigation
     fluidPage(
-      tags$div(
-        class = "centered-form",
-        f7Card(
-          title = h3("Record A New Catch"),
-          br(),
-          br(),
-          fluidRow(
-          f7List(
-            column(4,
+  # Include the external CSS file
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")
+  ),
+  
+  tags$div(
+    class = "centered-form",
+    f7Card(
+      title = h1("Record A New Catch"),
+      fluidRow(
+        f7List(
+          mode = "simple",
+          column(4,
             textInput("angler", "Your Name"),
             textInput("boat", "Boat Name"),
             shiny::dateInput("catch_date", "Date", value = current_date),
-            # selectizeInput("species", "Shark and Ray Species", choices = NULL, options = list(placeholder = 'Start typing...', create = TRUE)),
             selectizeInput("port", "Port of Use", choices = NULL, options = list(placeholder = 'Type or Select', create = TRUE)),
             selectizeInput("gear", "Fishing Gear", choices = NULL, options = list(placeholder = 'Type or Select', create = TRUE)),
-
-            numericInput("species_richness", "How many species did you catch?", value = NULL),
-
+            sliderInput("species_richness", "How many species did you catch?", min = 0, max = 50, value = 0, step = 1),
             numericInput("weight", "Weight of Total Catch (kg)", value = NULL),
             sliderInput("fishing_hours", "How many hours were you fishing?", min = 0, max = 24, value = 0, step = 0.5)
-            ),
-            column(4,
-            radioButtons("ais", "Are you using AIS/VMS?", width = "70%", choices = c("Yes" = "TRUE", "No" = "FALSE", "Not Sure" = "not sure"), selected = character(0)),
+          ),
+          column(4,
+            radioButtons("ais", "Are you using AIS/VMS?", width = "100%", choices = c("Yes" = "TRUE", "No" = "FALSE"), selected = character(0)),
             fileInput("file", "Upload an Image of Your Catch!",
-            accept = c("image/jpeg", "image/jpg", "image/png", "image/JPG",
-                        "image/JPEG", "image/heic", "image/HEIC", "image/heif", "image/HEIF")),
+              accept = c("image/jpeg", "image/jpg", "image/png", "image/JPG",
+                         "image/JPEG", "image/heic", "image/HEIC", "image/heif", "image/HEIF")),
             textAreaInput("notes", "Comments", "", rows = 5, cols = 40)
-            ),
-            column(4,
+          ),
+          column(4,
             h3("Where were you fishing?"),
             br(),
             numericInput("latitude", "Latitude", value = NULL),
             numericInput("longitude", "Longitude", value = NULL),
-            leafletOutput("map", height = 400))
+            leafletOutput("map", height = 300)
           )
-          ),
-          style = "font-size: 18px;"
         )
       ),
-      f7Button(inputId = "submit", label = "Submit")
+      style = "font-size: 18px;"
     )
+  ),
+  actionButton("submit", "Submit")
+)
+
   ),
   tabPanel(
     title = "Maps",
@@ -261,81 +167,221 @@ ui <- navbarPage(
     value = "Maps", # Value for tab navigation
 fluidPage(
   tabsetPanel(
-    tabPanel(h4("Fishing Activity"),
+    tabPanel(h5("Fishing Activity"),
              htmlOutput("fishing_activity_map")
     ),
-    tabPanel(h4("Fishing Effort"),
+    tabPanel(h5("Fishing Effort"),
              htmlOutput("fishing_effort_grid")
+    ),
+    tabPanel(h5("Water Temperature"),
+          htmlOutput("temperature_map")
+    ),
+    tabPanel(h5("Chlorophyll A"),
+          htmlOutput("chl_map")
+    ),
+    tabPanel(h5("Wave Height"),
+      htmlOutput("wave_map")
     )
   )
   )
-)
+),
+common_fish,
+tabPanel(
+      "Observers",
+      icon = icon("binoculars"),
+      htmlOutput("form_iframe")
+    ),
+tabPanel("User", 
+icon = icon("user"),
+uiOutput("auth_profile_ui"))
 )
 
 server <- function(input, output, session) {
 
+  output$form_iframe <- renderUI({
+    includeHTML("www/observer_form.html")
+  })
+
+  # Reactive value to store user information
+  user_data <- reactiveVal(NULL)
+  
+  # Combined Registration, Login, and Profile UI
+  output$auth_profile_ui <- renderUI({
+    if (is.null(user_data())) {
+      tagList(
+        selectInput("auth_type", "Choose Action:", choices = c("Login", "Register")),
+        conditionalPanel(
+          condition = "input.auth_type == 'Login'",
+          textInput("login_email", "Email"),
+          passwordInput("login_password", "Password"),
+          actionButton("login_btn", "Login")
+        ),
+        conditionalPanel(
+          condition = "input.auth_type == 'Register'",
+          textInput("register_email", "Email"),
+          radioButtons("stakeholder", "What profession do you identify with?", width = "70%", 
+                       choices = c("Fisher", "Manager", "Seafood Vendor", "Scientist", "Other"), selected = character(0)),
+          textInput("register_username", "Username"),
+          passwordInput("register_password", "Password"),
+          actionButton("register_btn", "Register")
+        )
+      )
+    } else {
+      tagList(
+        h3("User Profile"),
+        p(paste("Logged in as:", user_data()$username)),
+        p(paste(user_data()$stakeholder)),
+        img(src = paste0("icons/", user_data()$icon), height = "50px", style = "margin-top: 20px;"),
+        br(),
+        br(),
+        actionButton("logout_btn", "Logout")
+      )
+    }
+  })
+  
+  # Registration process
+observeEvent(input$register_btn, {
+  con <- connectMed_monitoring("med_monitoring")
+  on.exit(dbDisconnect(con))  # Ensure the connection is closed
+  
+  hashed_password <- digest(input$register_password, algo = "sha256")
+  icon <- switch(input$stakeholder,
+                 "Fisher" = "fisher.png",
+                 "Manager" = "manager.png",
+                 "Seafood Vendor" = "vendor.png",
+                 "Scientist" = "scientist.png",
+                 "Other" = "other.png",
+                 "other.png"  # Default case (also handles NA values)
+  )
+  
+  if (input$register_email == "" || input$register_username == "" || input$register_password == "") {
+    showNotification("Please fill in all fields.", type = "error")
+  } else {
+    tryCatch({
+      dbExecute(con, paste0("INSERT INTO tun_users (email, username, password, stakeholder, icon) VALUES ('",
+                            input$register_email, "', '", input$register_username, "', '", hashed_password, "', '", input$stakeholder, "', '", icon, "')"))
+      showNotification("Registration successful!", type = "message")
+    }, error = function(e) {
+      showNotification(paste("Registration failed:", e$message), type = "error")
+    })
+  }
+})
+
+  
+  # Login process
+  observeEvent(input$login_btn, {
+    credentials <- check_credentials(input$login_email, input$login_password)
+    
+    if (!is.null(credentials)) {
+      user_data(credentials)  # Store user information
+      showNotification("Login successful!", type = "message")
+    } else {
+      showNotification("Invalid email or password.", type = "error")
+    }
+  })
+  
+  # Logout process
+  observeEvent(input$logout_btn, {
+    user_data(NULL)
+    showNotification("You have logged out.", type = "message")
+  })
+  
+  # Display user information on the Home tab
+  output$user_info <- renderUI({
+    req(user_data())
+  tagList(
+    tags$p(
+      style = "display: flex; align-items: center;",
+      tags$span(paste("Logged in as:", user_data()$username)),
+      tags$img(src = paste0("icons/", user_data()$icon), height = "30px", style = "margin-left: 10px;")
+      )
+    )
+  })
+
+
   output$fishing_activity_map <- renderUI({
-      tags$iframe(src = "fishing_activity_map.html", width = "100%", height = "600px")
+      tags$iframe(src = "maps/fishing_activity_map.html", width = "100%", height = "600px")
     })
   
   output$fishing_effort_grid <- renderUI({
-    tags$iframe(src = "fishing_effort_grid.html", width = "100%", height = "600px")
+    tags$iframe(src = "maps/fishing_effort_grid.html", width = "100%", height = "600px")
+  })
+
+  output$temperature_map <- renderUI({
+    tags$iframe(src = "maps/tunisian_sst_map_celsius.html", width = "100%", height = "600px")
+  })
+
+  output$chl_map <- renderUI({
+    tags$iframe(src = "maps/tunisian_chl_map.html", width = "100%", height = "600px")
+  })
+
+  output$wave_map <- renderUI({
+    tags$iframe(src = "maps/tunisian_wave_map.html", width = "100%", height = "600px")
   })
 
   # Server-side selectize for species
   # updateSelectizeInput(session, 'species', choices = fetchSpeciesNames(), server = TRUE)
   updateSelectizeInput(session, 'port', choices = c("", as.character(tunisian_cities$port), "Other"), server = TRUE)
   updateSelectizeInput(session, 'gear', choices = c("", as.character(gear_list$gear), "Other"), server = TRUE)
-  observeEvent(input$submit, {
-    # Initialize lat and lon with NA
-    lat <- input$latitude
-    lon <- input$longitude
-    imageName <- NA
 
-    if (!is.null(input$file)) {
-      uploadedFile <- input$file
-      destPath <- paste0(getwd(), "/www/", uploadedFile$name)
-      file.rename(uploadedFile$datapath, destPath)
-      imageName <- as.character(uploadedFile$name)
-      
-      # Attempt to extract EXIF data
-      exifData <- read_exif(destPath)
-      
-      # Check if GPSLatitude and GPSLongitude exist in the exifData
-      if ("GPSLatitude" %in% names(exifData) && "GPSLongitude" %in% names(exifData)) {
-        lat <- exifData$GPSLatitude
-        lon <- exifData$GPSLongitude
+ observeEvent(input$submit, {
+      # session$reload()
+      # Initialize lat and lon with NA
+      ais = if (!is.null(input$ais) && input$ais != "") input$ais else NA
+      lat = if (!is.null(input$latitude)) input$latitude else NA
+      lon = if (!is.null(input$longitude)) input$longitude else NA
+      imageName <- NA
+      port = if (!is.null(input$port) && input$port != "") input$port else NA
+      gear = if (!is.null(input$gear) && input$gear != "") input$gear else NA
+      angler = if (!is.null(input$angler) && input$angler != "") input$angler else NA
+      boat = if (!is.null(input$boat) && input$boat != "") input$boat else NA
+      notes = if (!is.null(input$notes) && input$notes != "") input$notes else NA
+      species_richness = if (input$species_richness != 0) input$species_richness else NA
+      fishing_hours = if (input$fishing_hours != 0) input$fishing_hours else NA
+
+      if (!is.null(input$file)) {
+        uploadedFile <- input$file
+        destPath <- paste0(getwd(), "/www/submissions/", uploadedFile$name)
+        file.rename(uploadedFile$datapath, destPath)
+        imageName <- as.character(uploadedFile$name)
+        
+        # Attempt to extract EXIF data
+        exifData <- read_exif(destPath)
+        
+        # Check if GPSLatitude and GPSLongitude exist in the exifData
+        if ("GPSLatitude" %in% names(exifData) && "GPSLongitude" %in% names(exifData)) {
+          lat <- exifData$GPSLatitude
+          lon <- exifData$GPSLongitude
+        }
       }
-    }
 
-    new_entry <- data.frame(
-      date = as.Date(input$catch_date),
-      port = input$port,
-      # species = input$species,
-      species_richness = as.numeric(input$species_richness),
-      # fl = as.numeric(input$fl),
-      weight = as.numeric(input$weight),
-      img_name = imageName,
-      gear = input$gear,
-      fishing_hours = as.numeric(input$fishing_hours),
-      latitude = lat,
-      longitude = lon,
-      angler = input$angler,
-      boat = input$boat,
-      notes = input$notes,
-      ais = input$ais,
-      stringsAsFactors = FALSE
-    )
+      new_entry <- data.frame(
+        date = as.Date(input$catch_date),
+        port = port,
+        species_richness = species_richness,
+        weight = input$weight,
+        img_name = imageName,
+        gear = gear,
+        fishing_hours = fishing_hours,
+        latitude = lat,
+        longitude = lon,
+        angler = angler,
+        boat = boat,
+        notes = notes,
+        ais = ais,
+        stringsAsFactors = FALSE
+      )
+      # write.csv(new_entry, "/srv/shiny-server/comanage/test.csv", row.names = FALSE)
 
-    con <- connectPelagic()
-    tryCatch({
-      dbWriteTable(con, "catch_log", new_entry, append = TRUE, row.names = FALSE)
-      dbDisconnect(con)
-      shiny::showNotification("Catch logged successfully!", type = "message")
-    }, error = function(e) {
-      shiny::showNotification(as.character(e), type = "error")
+      con <- connectMed_monitoring("med_monitoring")
+      tryCatch({
+        dbWriteTable(con, "tun_catch", new_entry, append = TRUE, row.names = FALSE)
+        dbDisconnect(con)
+        shiny::showNotification("Catch logged successfully!", type = "message")
+      }, error = function(e) {
+        shiny::showNotification(as.character(e), type = "error")
+      })
     })
-  })
 
 output$map <- renderLeaflet({
   leaflet() %>%
